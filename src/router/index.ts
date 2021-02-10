8519a2e9495e9a2d5059e72eebffec9b1ac9cdd8
rain2o <joel.rain2o@gmail.com>
2021-02-10 19:22:21 +0100

    Handle 404 and netlify redirects

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,10 +13,12 @@ const routes: Array<RouteConfig> = [
   {
     path: '/survey/:p?',
     name: 'Survey',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "survey" */ '../views/Survey.vue'),
+  },
+  {
+    // Catch-all for 404 handler
+    path: '*',
+    component: () => import(/* webpackChunkName: "notfound" */ '../views/NotFound.vue'),
   },
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<script>
+/**
+ * We don't show the 404, but instead redirect to home page
+ */
+export default {
+  name: 'NotFound',
+  beforeRouteEnter(_to, _from, next) {
+    next('/');
+  },
+};
+</script>
