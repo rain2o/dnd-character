bfa56aaedd134dc6c75e3951fd11b386b2ddea8a
rain2o <joel.rain2o@gmail.com>
2021-02-11 00:55:58 +0100

    Create ability score components

---
+++ src/components/AbilityScore.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ability-score">
+    <div class="border-secondary border-2 rounded text-center p-1">
+      <p class="text-gray-600 mb-2">{{ name }}</p>
+      <p class="text-lg font-semibold text-secondary relative -bottom-1">{{ score }}</p>
+      <p
+        class="text-secondary text-sm border rounded-full border-secondary w-10 mx-auto
+              -bottom-4 relative bg-background py-1"
+      >
+        {{ modifier }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'AbilityScore',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    score: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    modifier(): number {
+      return Math.floor((this.score - 10) / 2);
+    },
+  },
+});
+</script>
---
+++ src/components/AbilityScores.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-wrap justify-evenly">
+    <AbilityScore name="Strength" :score="abilities.strength" class="w-1/3 p-1 mb-3" />
+    <AbilityScore name="Dexterity" :score="abilities.dexterity" class="w-1/3 p-1 mb-3" />
+    <AbilityScore name="Constitution" :score="abilities.constitution" class="w-1/3 p-1 mb-3" />
+    <AbilityScore name="Intelligence" :score="abilities.intelligence" class="w-1/3 p-1 mb-3" />
+    <AbilityScore name="Wisdom" :score="abilities.wisdom" class="w-1/3 p-1 mb-3" />
+    <AbilityScore name="Charisma" :score="abilities.charisma" class="w-1/3 p-1 mb-3" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { AbilityScoreScores } from '@/types';
+import AbilityScore from './AbilityScore.vue';
+
+export default Vue.extend({
+  name: 'AbilityScores',
+  components: {
+    AbilityScore,
+  },
+  props: {
+    abilities: {
+      type: Object as () => AbilityScoreScores,
+      required: true,
+    },
+  },
+});
+</script>
