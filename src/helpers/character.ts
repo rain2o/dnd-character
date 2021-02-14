e18fc03988b4bf3e925b46acc57938e0b79d3aea
rain2o <joel.rain2o@gmail.com>
2021-02-14 12:42:49 +0100

    pick random winner for ties

--- src/helpers/character.ts
+++ src/helpers/character.ts
@@ -4,11 +4,53 @@ import {
   ClassName,
   Race,
   Scores,
+  Stat,
+  StatScores,
 } from '@/types';
 
 export const DEFAULT_RACE: Race = 'human';
 export const DEFAULT_CLASS: ClassName = 'fighter';
 export const DEFAULT_ALIGNMENT: Alignment = 'nn';
+
+/**
+ * Find top scores in the stat scores provided.
+ * @param statScores  Scores of a specific stat
+ * @param defaultStat Default stat if none are found
+ */
+export const findTopScores = (statScores: StatScores, defaultStat: Stat|null = null): Stat[] => {
+  // map object keys to an array of scores for easier use
+  const scoresArray = (Object.keys(statScores) as Stat[]).map((stat) => ({
+    stat,
+    value: statScores[stat] || 0,
+  })).filter((score) => score.value > 0); // filter all scores over 0
+
+  // find top score value
+  const maxScore = Math.max(...scoresArray.map((score) => score.value));
+  // filter by all stats with top score
+  const topStats = scoresArray.filter((score) => score.value === maxScore);
+
+  // return all top stats
+  if (topStats.length > 0) {
+    return topStats.map((score) => score.stat);
+  }
+  // if no results, return empty array or default if provided
+  return (defaultStat === null) ? [] : [defaultStat];
+};
+
+/**
+ * Pick a random winner from the array of stats.
+ * Can optionally exclude a stat from the results.
+ * @param stats   Array of stats to choose from
+ * @param exclude Specific stat to exclude from result
+ */
+export const pickRandomWinner = (stats: Stat[], exclude?: Stat): Stat => {
+  // skip our logic for random if it's just one item in the array
+  if (stats.length === 1) {
+    return stats[0];
+  }
+  const winner = stats[Math.floor(Math.random() * stats.length)];
+  return (winner === exclude) ? pickRandomWinner(stats, winner) : winner;
+};
 
 /**
  * Build the character object using the final scores
@@ -24,22 +66,16 @@ export const buildCharacter = (scores: Scores) => {
   } = scores;
 
   // Race
-  const filteredRaces = (Object.keys(races) as Race[])
-    .filter((race) => races[race] > 0);
-  const race: Race = filteredRaces.length
-    ? filteredRaces.reduce((a, b) => (races[a] > races[b] ? a : b))
-    : DEFAULT_RACE;
+  const topRaces = findTopScores(races, DEFAULT_RACE) as Race[];
+  const race = pickRandomWinner(topRaces) as Race;
 
   // Primary Class
-  const filteredClasses = (Object.keys(races) as ClassName[])
-    .filter((className) => classes[className] > 0);
-  const primaryClass: ClassName = filteredClasses.length
-    ? filteredClasses.reduce((a, b) => (classes[a] > classes[b] ? a : b))
-    : DEFAULT_CLASS;
+  const topClasses = findTopScores(classes, DEFAULT_CLASS) as ClassName[];
+  const primaryClass = pickRandomWinner(topClasses) as ClassName;
 
   // Secondary Class
-  const secondaryClass: ClassName|null = filteredClasses.length
-    ? filteredClasses.reduce((a, b) => (classes[a] > classes[b] ? a : b))
+  const secondaryClass = (topClasses.length > 1)
+    ? pickRandomWinner(topClasses, primaryClass) as ClassName
     : null;
 
   const characterClasses: ClassName[] = [primaryClass];
@@ -48,11 +84,8 @@ export const buildCharacter = (scores: Scores) => {
   }
 
   // Alignment
-  const filteredAlignments = (Object.keys(alignments) as Alignment[])
-    .filter((alignment) => alignments[alignment] > 0);
-  const alignment: Alignment = filteredAlignments.length
-    ? filteredAlignments.reduce((a, b) => (alignments[a] > alignments[b] ? a : b))
-    : DEFAULT_ALIGNMENT;
+  const topAlignments = findTopScores(alignments, DEFAULT_ALIGNMENT) as Alignment[];
+  const alignment = pickRandomWinner(topAlignments) as Alignment;
 
   const character: Character = {
     race,
